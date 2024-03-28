8c2c9319fe1cd11d5ffc9c2865db67e660750693
arthur <arthur.rios007@gmail.com>
2024-03-28 16:29:02 -0300

    Aula 03

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -5,16 +5,36 @@ import { MaterialCommunityIcons } from '@expo/vector-icons'
 
 import { colors } from '@/styles/colors'
 import { Button } from '@/components/button'
-import { Link } from 'expo-router'
+import { Link, Redirect } from 'expo-router'
 import { useState } from 'react'
+import { api } from '@/server/api'
+import { useBadgeStore } from '@/store/badge-store'
 
 export default function Home() {
   const [code, setCode] = useState('')
+  const [isLoading, setIsLoading] = useState(false)
 
-  function handleAccessCredential() {
-    if (!code.trim()) {
-      return Alert.alert('Ingresso', 'Informe o código do ingresso')
+  const badgeStore = useBadgeStore()
+
+  async function handleAccessCredential() {
+    try {
+      if (!code.trim()) {
+        return Alert.alert('Ingresso', 'Informe o código do ingresso')
+      }
+
+      const { data } = await api.get(`attendees/${code}/badge`)
+      badgeStore.save(data.badge)
+
+      setIsLoading(true)
+    } catch (error) {
+      console.log(error)
+      setIsLoading(false)
+      Alert.alert('Ingresso', 'Ingresso não encontrado')
     }
+  }
+
+  if (badgeStore.data?.checkInURL) {
+    return <Redirect href="/ticket" />
   }
 
   return (
@@ -41,7 +61,11 @@ export default function Home() {
           />
         </Input>
 
-        <Button title="Acessar credencial" onPress={handleAccessCredential} />
+        <Button
+          title="Acessar credencial"
+          onPress={handleAccessCredential}
+          isLoading={isLoading}
+        />
 
         <Link
           href="/register"
